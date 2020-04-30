b0503e99425e9558c5ab57126e6253116478e559
n1tjrgns <n2tjrgns@gmail.com>
2020-04-30 16:59:31 +0900

    주문, 주문상품(재고처리) 엔티티 개발

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -57,4 +57,46 @@ public class Order {
         member.getOrders().add(order);
         order.setMember(member);
     }*/
+
+    /*생성 메소드*/
+    //... 으로 여러개 넘길 수 있다 문법이라고 함
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /*비즈니스 로직*/
+    // 주문 취소
+    public void cancel(){
+        //배송 완료된 상태이면
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    /*조회 로직*/
+    //전체 주문 가격 조회
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+    }
+
+
 }
--- src/main/java/com/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/com/jpabook/jpashop/domain/OrderItem.java
@@ -27,4 +27,27 @@ public class OrderItem {
     private int orderPrice; //주문 가격
 
     private int count; //주문 수량
+
+    /*생성 메소드*/
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        //아이템이 주문되면 재고를 깎는다다
+       item.removeStock(count);
+        return orderItem;
+    }
+
+
+    /*비즈니스 로직*/
+    public void cancel() {
+        //취소하면 재고수량 ++
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
