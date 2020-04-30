1945c5371673db968389f3c0db3f6386516c4c7a
n1tjrgns <n2tjrgns@gmail.com>
2020-04-30 16:30:18 +0900

    상품 재고 엔티티 추가, Excepiton추가

--- src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,8 @@
 package com.jpabook.jpashop.domain.item;
 
 import com.jpabook.jpashop.domain.Category;
+import com.jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -12,7 +12,7 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 전략
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
+//@Setter
 public abstract class Item {
 
     @Id
@@ -28,4 +28,24 @@ public abstract class Item {
     @ManyToMany
     @JoinTable(name = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /*비즈니스 로직*/
+
+    //setter를 사용하지 않고 아래처럼 별도의 메소드로 작업을 해야한다.
+
+    //해당 데이터를 가지고 있는 클래스에서 비즈니스 로직이 출발해야 좋다
+    // -> 응집력 증가
+    //재고 수량 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    //재고 수량 감소
+    public void removeStock(int ququantity){
+        int restStock = this.stockQuantity - ququantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/main/java/com/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,25 @@
+﻿package com.jpabook.jpashop.exception;
+
+public class NotEnoughStockException extends RuntimeException{
+
+    //오버라이드 메소드
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
