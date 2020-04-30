10233220d42d6ed430853fc4e494a8ffaad3d76a
n1tjrgns <n2tjrgns@gmail.com>
2020-04-30 16:35:51 +0900

    상품 repository 개발

---
+++ src/main/java/com/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+﻿package com.jpabook.jpashop.repository;
+
+import com.jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    //jpa에 저장하기전까지 item의 id는 없기 때문에 save와 update를 구분함
+    public void save(Item item){
+        if(item.getId() == null){
+            em.persist(item);
+        }else{
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
