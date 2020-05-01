2adeb72a157350d73b280ff6ead45a7676677b2c
n1tjrgns <n2tjrgns@gmail.com>
2020-05-01 14:46:59 +0900

    주문 service 개발

---
+++ src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,61 @@
+﻿package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Delivery;
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.domain.item.Item;
+import com.jpabook.jpashop.repository.ItemRepository;
+import com.jpabook.jpashop.repository.MemberRepository;
+import com.jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    //주문, 회원 id 아이템 id 수량을 받아옴
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress()); //예제를 위해 간단하게 저장된 정보로 배송정보 등록
+
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+        // 누군가 new OrderItem을 생성해서 set을 하면 로직이 중구난방이여서 섞일 수 있음. 그래서 막아놔야함.
+
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order); //전파옵션이 설정되어있기때문에 order만 저장해도 배달, 주문상품이 같이 저장됨
+
+        return order.getId();
+    }
+
+    //주문 취소
+    @Transactional
+    public void cancelOrder(Long orderId){
+
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
+
+    //검색
+}
