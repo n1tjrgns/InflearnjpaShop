b464a295cccd49ca55b49d2a4ef549d4d281c447
n1tjrgns <n2tjrgns@gmail.com>
2020-05-01 17:48:24 +0900

    상품 수정 기능 개발

--- src/main/java/com/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -42,5 +44,41 @@ public class ItemController {
         model.addAttribute("items", items);
 
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
+        //원래는 아이템으로 받아서 타입별로 가져와야하지만 간단한 예제 book만을 가져온다고 가정해서 캐스팅
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    //뷰에서 넘긴 폼을 받는다 @ModelAttribute
+    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form){
+
+        //post 방식으로 id를 넘겨줄때 웹 url에 injection공격을 당할 수 있기 때문에
+        //서버에서 권한을 체크하는 로직을 넣어야한다.
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
