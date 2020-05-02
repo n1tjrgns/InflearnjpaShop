5406c626e9078483b0abf8db473e530e8d0d09ea
n1tjrgns <n2tjrgns@gmail.com>
2020-05-02 16:13:09 +0900

    간단한 주문 조회 V2 버전(엔티티 클래스를 DTO로 변환, N+1 문제 발생)

--- src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,12 +1,17 @@
 package com.jpabook.jpashop.api;
 
+import com.jpabook.jpashop.domain.Address;
 import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderStatus;
 import com.jpabook.jpashop.repository.OrderRepository;
 import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 /* Order
@@ -31,5 +36,35 @@ public class OrderSimpleApiController {
         List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
 
         return all;
+    }
+
+
+    //V2 -> DTO로 변환
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> result = new ArrayList<>();
+        for (Order o : orders) {
+            SimpleOrderDto simpleOrderDto = new SimpleOrderDto(o);
+            result.add(simpleOrderDto);
+        }
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = getOrderStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
